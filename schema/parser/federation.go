6a9fbde40e5902ad4493d235244f34b35385199e
Water.Li <lizongbin@hotmail.com>
2023-05-08 16:57:55 +0800

    开始添加Federation

---
+++ schema/parser/federation.go
@@ -0,0 +1,30 @@
+package parser
+
+import (
+	"fmt"
+)
+
+var allSDL = `
+extend schema
+	@link(url: "https://specs.apollo.dev/federation/v2.0",
+		import: ["@key"])
+
+scalar JSON
+scalar DateTime
+
+type Query {
+%s
+}
+
+type Mutation {
+%s
+}
+%s
+`
+
+func makeFederationSDL() string {
+	sdl := allSDL
+	queryFields, queryTypes := querySDL()
+	mutationFields, mutationTypes := mutationSDL()
+	return fmt.Sprintf(sdl, queryFields, mutationFields, queryTypes+mutationTypes)
+}
---
+++ schema/parser/federationquery.go
@@ -0,0 +1,222 @@
+package parser
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/codebdy/entify-graphql-schema/consts"
+	"github.com/codebdy/entify/model/graph"
+	"github.com/graphql-go/graphql"
+)
+
+var queryFieldSDL = "\t%s(%s) : %s \n"
+
+var objectWithKeySDL = `
+type %s%s @key(fields: "id"){
+	%s
+}
+`
+
+var objectSDL = `
+type %s%s {
+	%s
+}
+`
+
+var enumSDL = `
+enum %s{
+	%s
+}
+`
+
+var inputSDL = `
+input %s{
+	%s
+}
+`
+
+var comparisonSDL = `
+input %s{
+	%s
+}
+`
+
+func (p *ModelParser) QuerySDL() (string, string) {
+	queryFields := ""
+	types := ""
+	for _, enum := range p.model.Graph.Enums {
+		types = types + enumToSDL(p.EnumType(enum.Name))
+	}
+
+	for _, enum := range p.DistinctOnEnums() {
+		types = types + enumToSDL(enum)
+	}
+
+	types = types + enumToSDL(EnumOrderBy)
+	for _, orderBy := range p.orderByMap {
+		types = types + inputToSDL(orderBy)
+	}
+
+	types = types + comparisonToSDL(&BooleanComparisonExp)
+	types = types + comparisonToSDL(&DateTimeComparisonExp)
+	types = types + comparisonToSDL(&FloatComparisonExp)
+	types = types + comparisonToSDL(&IntComparisonExp)
+	types = types + comparisonToSDL(&IdComparisonExp)
+	types = types + comparisonToSDL(&StringComparisonExp)
+
+	for _, comparision := range p.enumComparisonExpMap {
+		types = types + comparisonToSDL(comparision)
+	}
+
+	for _, where := range p.whereExpMap {
+		types = types + inputToSDL(where)
+	}
+
+	for _, entity := range p.model.Graph.Entities {
+		types = types + objectToSDL(p.EntityeOutputType(entity.Name()), true)
+	}
+	for _, entity := range p.model.Graph.RootEnities() {
+		queryFields = queryFields + makeEntitySDL(entity)
+	}
+
+	for _, aggregate := range p.aggregateMap {
+		types = types + objectToSDL(aggregate, false)
+		fieldsType := aggregate.Fields()[consts.AGGREGATE].Type.(*graphql.Object)
+		types = types + objectToSDL(fieldsType, false)
+
+		for key := range fieldsType.Fields() {
+			field := fieldsType.Fields()[key]
+			if field.Name != consts.ARG_COUNT {
+				types = types + objectToSDL(field.Type.(*graphql.Object), false)
+			}
+		}
+	}
+
+	for _, selectColumn := range p.selectColumnsMap {
+		types = types + inputToSDL(selectColumn)
+	}
+	return queryFields, types
+}
+
+
+func makeEntitySDL(entity *graph.Entity) string {
+	sdl := ""
+	sdl = sdl + fmt.Sprintf(queryFieldSDL,
+		entity.QueryName(),
+		makeArgsSDL(queryArgs(entity.Name())),
+		queryResponseType(&entity.Class).String(),
+	)
+
+	sdl = sdl + fmt.Sprintf(queryFieldSDL,
+		entity.QueryOneName(),
+		makeArgsSDL(queryArgs(entity.Name())),
+		Cache.OutputType(entity.Name()).String(),
+	)
+
+	sdl = sdl + fmt.Sprintf(queryFieldSDL,
+		entity.QueryAggregateName(),
+		makeArgsSDL(queryArgs(entity.Name())),
+		(*AggregateEntityType(entity)).String(),
+	)
+
+	return sdl
+}
+
+func makeArgsSDL(args graphql.FieldConfigArgument) string {
+	var sdls []string
+	for key := range args {
+		sdls = append(sdls, key+":"+args[key].Type.Name())
+	}
+	return strings.Join(sdls, ",")
+}
+
+func makeArgArraySDL(args []*graphql.Argument) string {
+	var sdls []string
+	for _, arg := range args {
+		sdls = append(sdls, arg.Name()+":"+arg.Type.Name())
+	}
+	return strings.Join(sdls, ",")
+}
+
+func makeAuthSDL() string {
+	return fmt.Sprintf("\tme : %s \n", baseUserType.Name())
+}
+
+func serviceField() *graphql.Field {
+	return &graphql.Field{
+		Type: _ServiceType,
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			defer utils.PrintErrorStack()
+			return map[string]interface{}{}, nil
+		},
+	}
+}
+
+func objectToSDL(obj *graphql.Object, withKey bool) string {
+	var intfNames []string
+	implString := ""
+
+	for _, intf := range obj.Interfaces() {
+		intfNames = append(intfNames, intf.Name())
+	}
+	if len(intfNames) > 0 {
+		implString = " implements " + strings.Join(intfNames, " & ")
+	}
+
+	sdl := objectSDL
+	if withKey {
+		sdl = objectWithKeySDL
+	}
+	return fmt.Sprintf(sdl, obj.Name(), implString, fieldsToSDL(obj.Fields()))
+}
+
+func enumToSDL(enum *graphql.Enum) string {
+	var values []string
+
+	sdl := enumSDL
+	for _, value := range enum.Values() {
+		values = append(values, value.Name)
+	}
+	return fmt.Sprintf(sdl, enum.Name(), strings.Join(values, "\n\t"))
+}
+
+func interfaceToSDL(intf *graphql.Interface) string {
+	sdl := interfaceSDL
+	return fmt.Sprintf(sdl, intf.Name(), fieldsToSDL(intf.Fields()))
+}
+
+func inputToSDL(input *graphql.InputObject) string {
+	sdl := inputSDL
+	return fmt.Sprintf(sdl, input.Name(), inputFieldsToSDL(input.Fields()))
+}
+
+func inputFieldsToSDL(fields graphql.InputObjectFieldMap) string {
+	var fieldsStrings []string
+	for key := range fields {
+		field := fields[key]
+		fieldsStrings = append(fieldsStrings, key+":"+field.Type.String())
+	}
+
+	return strings.Join(fieldsStrings, "\n\t")
+}
+
+func comparisonToSDL(comarison *graphql.InputObjectFieldConfig) string {
+	sdl := comparisonSDL
+	var comType *graphql.InputObject
+	comType = comarison.Type.(*graphql.InputObject)
+	return fmt.Sprintf(sdl, comType.Name(), inputFieldsToSDL(comType.Fields()))
+}
+
+func fieldsToSDL(fields graphql.FieldDefinitionMap) string {
+	var fieldsStrings []string
+	for i := range fields {
+		field := fields[i]
+		if len(field.Args) > 0 {
+			fieldsStrings = append(fieldsStrings, fmt.Sprintf("%s(%s):%s", field.Name, makeArgArraySDL(field.Args), field.Type.String()))
+		} else {
+			fieldsStrings = append(fieldsStrings, field.Name+":"+field.Type.String())
+		}
+	}
+
+	return strings.Join(fieldsStrings, "\n\t")
+}
