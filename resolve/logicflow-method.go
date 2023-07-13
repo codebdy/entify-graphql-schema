ec218b48215f4e5449023639113fb0714c13404c
Water.Li <lizongbin@hotmail.com>
2023-07-13 13:53:06 +0800

    Update logicflow-method.go

--- resolve/logicflow-method.go
+++ resolve/logicflow-method.go
@@ -14,9 +14,16 @@ import (
 func LogicFlowMethodResolveFn(method *meta.MethodMeta, repository *entify.Repository, subFlows *[]dsl.SubLogicFlowMeta) graphql.FieldResolveFn {
 	return func(p graphql.ResolveParams) (interface{}, error) {
 		defer shared.PrintErrorStack()
+		var returnValue any
+		ctx := runtime.AttachSubFlowsToContext(subFlows, context.Background())
+		logicFlow := runtime.NewLogicflow(method.LogicMeta, ctx)
 
-		logicFlow := runtime.NewLogicflow(method.LogicMeta, runtime.AttachSubFlowsToContext(subFlows, context.Background()) )
+		logicFlow.Jointers.GetSingleOutput().Connect(func(inputValue any, ctx context.Context) {
+			returnValue = inputValue
+		})
 
-		return nil, nil
+		//后端args是map，前端是数组。后端无法保证参数顺序，前端无法获知参数名字
+		logicFlow.Jointers.GetSingleInput().Push(p.Args, ctx)
+		return returnValue, nil
 	}
 }
